d3c4549a142fadec47abfab6a601d7147cdab618
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-22 16:52:25 -0400

    failed attempt to fix new line in create adevnture

--- client/src/components/CreateCharacter.jsx
+++ client/src/components/CreateCharacter.jsx
@@ -10,10 +10,12 @@ export default class CreateCharacter extends Component {
     super();
     this.state = {
       heading: "Backstory",
-      placeholder: "Gandalf was born in the year 2953 of the Third Age. He was originally a Maia of the race of the Valar, and his name was Olorin. He was one of the Maiar who remained in Middle-earth after the Valar departed. He became a friend of the Elves of Middle-earth and often visited them. He was known by various names, including Mithrandir, meaning \"Gray Pilgrim\", and tharkûn, meaning \"staff-man\". In the year 2063 of the Third Age, he took the form of an old man and went to live among the Dwarves of the Blue Mountains. He remained with them for many years, and became known as Gandalf the Grey.\nIn the year 2941 of the Third Age, Gandalf returned to Middle-earth. He gathered the Dwarves of the Lonely Mountain and helped them reclaim their homeland from the dragon Smaug. He also played a key role in the War of the Ring, which culminated in the destruction of the One Ring and the defeat of Sauron. After the war, Gandalf was instrumental in the establishment of the Shire as a safe haven for the Hobbits. He also helped to restore the kingdom of Gondor.",
+      placeholder:
+        'Gandalf was born in the year 2953 of the Third Age. He was originally a Maia of the race of the Valar, and his name was Olorin. He was one of the Maiar who remained in Middle-earth after the Valar departed. He became a friend of the Elves of Middle-earth and often visited them. He was known by various names, including Mithrandir, meaning "Gray Pilgrim", and tharkûn, meaning "staff-man". In the year 2063 of the Third Age, he took the form of an old man and went to live among the Dwarves of the Blue Mountains. He remained with them for many years, and became known as Gandalf the Grey.\nIn the year 2941 of the Third Age, Gandalf returned to Middle-earth. He gathered the Dwarves of the Lonely Mountain and helped them reclaim their homeland from the dragon Smaug. He also played a key role in the War of the Ring, which culminated in the destruction of the One Ring and the defeat of Sauron. After the war, Gandalf was instrumental in the establishment of the Shire as a safe haven for the Hobbits. He also helped to restore the kingdom of Gondor.',
       newCharacter: {},
       character: "",
       log: [],
+      prompt: "",
     };
   }
 
@@ -58,15 +60,15 @@ export default class CreateCharacter extends Component {
   appendResponse = (response) => {};
 
   generateBackstory = (e) => {
-    console.log('CREATING BACKSTORY')
+    console.log("CREATING BACKSTORY");
     e.preventDefault();
 
     const formData = new FormData(e.target.parentNode),
       formDataObj = Object.fromEntries(formData.entries());
     console.log(formDataObj);
 
-    let AIprompt = `${formDataObj.name} is a ${formDataObj.class} who has the power of ${formDataObj.ability} and a weakness to ${formDataObj.weakness}. Write a detailed and ${formDataObj.tone} back story about ${formDataObj.name} in 100 words.\n`
-
+    let AIprompt = `${formDataObj.name} is a ${formDataObj.class} who has the power of ${formDataObj.ability} and a weakness to ${formDataObj.weakness}. Write a detailed and ${formDataObj.tone} back story about ${formDataObj.name} in 100 words.\n`;
+    this.setState({prompt:AIprompt})
     ////Open Ai Goes here
 
     const configuration = new Configuration({
@@ -85,8 +87,8 @@ export default class CreateCharacter extends Component {
       })
       .then((response) => {
         let backstory = response.data.choices[0].text;
-        if (backstory[0] =="\n") {
-          backstory = backstory.slice(1,backstory.length)
+        if (backstory[0] === "\n") {
+          backstory = backstory.slice(1, backstory.length);
         }
         this.setState({
           heading: `Backstory for: ${formDataObj.name}`,
@@ -99,7 +101,9 @@ export default class CreateCharacter extends Component {
       });
     this.setState({
       response: "",
-      placeholder: `Generating a ${formDataObj.tone.toLowerCase()} backstory for ${formDataObj.name}. Please wait...`,
+      placeholder: `Generating a ${formDataObj.tone.toLowerCase()} backstory for ${
+        formDataObj.name
+      }. Please wait...`,
     });
     //Use a timeout/clock here to randomly change state.response to keep things interesting while the AI thinks?
   };
@@ -112,14 +116,14 @@ export default class CreateCharacter extends Component {
     // console.log(formDataObj);
     // console.log(this.state.newCharacter);
 
-    this.addCharacter(formDataObj); 
+    this.addCharacter(formDataObj);
   };
 
   render() {
-    const entries = this.state.log.map((entry) => {
+    const entries = this.state.log.map((entry, index) => {
       return (
         <Card.Text>
-          <Entry text={entry} />
+          <Entry text={entry} key={index} />
         </Card.Text>
       );
     });
@@ -129,90 +133,84 @@ export default class CreateCharacter extends Component {
         <Container>
           <h1>Create a Character</h1>
 
-          <h4 className="text-white">
-            Define your character
-          </h4>
+          <h4 className='text-white'>Define your character</h4>
 
           <Form onSubmit={this.onFormSubmit}>
-          <Form.Group className='mb-3' controlId=''>
-            <Form.Label className="text-white">Character name</Form.Label>
-            <Form.Control
-              type='text'
-              name='name'
-              placeholder='Gandalf'
-              onChange={this.handleChange}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group className='mb-3' controlId=''>
-            <Form.Label className="text-white">Class/role/occupation</Form.Label>
-            <Form.Control
-              type='text'
-              name='class'
-              placeholder='Wizard'
-              onChange={this.handleChange}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group className='mb-3' controlId=''>
-            <Form.Label className="text-white">Special ability</Form.Label>
-            <Form.Control
-              type='text'
-              name='ability'
-              placeholder='Speak in riddles'
-              onChange={this.handleChange}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group className='mb-3' controlId=''>
-            <Form.Label className="text-white">Weakness</Form.Label>
-            <Form.Control
-              type='text'
-              name='weakness'
-              placeholder='Hobbits'
-              onChange={this.handleChange}
-            ></Form.Control>
-          </Form.Group>
-            <br></br>
-            <h4 className="text-white">
-              Write a short backstory, or let OpenAI do the work for you!
-            </h4>
-            <p className="text-white">
-              Tip: you can edit the AI-generated story directly in the textbox below (not working yet).
-            </p>
-            {/* <Form.Group className='mb-3' controlId='formBasicEmail'>
-              <Form.Label className="text-white">Who should we create a back story for?</Form.Label>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Character name</Form.Label>
               <Form.Control
-                text='text'
+                type='text'
                 name='name'
-                placeholder='Character Name + Description'
+                placeholder='Gandalf'
                 onChange={this.handleChange}
               ></Form.Control>
-              <Form.Text className="text-White">
-                Enter as much information as possible for a more detailed story.
-              </Form.Text>
-            </Form.Group> */}
+            </Form.Group>
             <Form.Group className='mb-3' controlId=''>
-              <Form.Label className="text-white">Tone</Form.Label>
+              <Form.Label className='text-white'>
+                Class/role/occupation
+              </Form.Label>
+              <Form.Control
+                type='text'
+                name='class'
+                placeholder='Wizard'
+                onChange={this.handleChange}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Special ability</Form.Label>
+              <Form.Control
+                type='text'
+                name='ability'
+                placeholder='Speak in riddles'
+                onChange={this.handleChange}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Weakness</Form.Label>
+              <Form.Control
+                type='text'
+                name='weakness'
+                placeholder='Hobbits'
+                onChange={this.handleChange}
+              ></Form.Control>
+            </Form.Group>
+            <br></br>
+            <h4 className='text-white'>
+              Write a short backstory, or let OpenAI do the work for you!
+            </h4>
+            <p className='text-white'>
+              Tip: you can edit the AI-generated story directly in the textbox
+              below (not working yet).
+            </p>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Tone</Form.Label>
               <Form.Select name='tone' onChange={this.handleChange}>
-                <option value="Dark">Dark</option>
-                <option value="Dry">Dry</option>
-                <option value="Grandiose">Grandiose</option>
-                <option value="Happy">Happy</option>
-                <option value="Humorous">Humorous</option>
-                <option value="Lighthearted">Lighthearted</option>
-                <option value="Lofty">Lofty</option>
-                <option value="Realistic">Realistic</option>
-                <option value="Sad">Sad</option>
-                <option value="Sarcastic">Sarcastic</option>
-                <option value="Serious">Serious</option>
-                <option value="Tragic">Tragic</option>
+                <option value='Dark'>Dark</option>
+                <option value='Dry'>Dry</option>
+                <option value='Grandiose'>Grandiose</option>
+                <option value='Happy'>Happy</option>
+                <option value='Humorous'>Humorous</option>
+                <option value='Lighthearted'>Lighthearted</option>
+                <option value='Lofty'>Lofty</option>
+                <option value='Realistic'>Realistic</option>
+                <option value='Sad'>Sad</option>
+                <option value='Sarcastic'>Sarcastic</option>
+                <option value='Serious'>Serious</option>
+                <option value='Tragic'>Tragic</option>
               </Form.Select>
             </Form.Group>
-            <Button variant='primary' size='lg' type='button' onClick={this.generateBackstory}>
+            <Button
+              variant='primary'
+              size='lg'
+              type='button'
+              onClick={this.generateBackstory}
+            >
               Generate Backstory
             </Button>
             <br></br>
             <br></br>
             <Form.Group className='mb-3' controlId=''>
-              <Form.Label className="text-white">Backstory</Form.Label>
+              <Form.Label className='text-white'>Backstory</Form.Label>
               <Form.Control
                 as='textarea'
                 value={this.state.response}
@@ -221,24 +219,13 @@ export default class CreateCharacter extends Component {
                 onChange={this.handleChange}
               ></Form.Control>
             </Form.Group>
-
-
             <Button variant='primary' size='lg' type='submit'>
               Create Character
             </Button>
           </Form>
           <br />
-            <br></br>
-            <br></br>
-
-          {/* <Card>
-            <Card.Body>
-              <Card.Title>{this.state.heading}</Card.Title>
-              <hr />
-              <Card.Text>{this.state.response}</Card.Text>
-            </Card.Body>
-          </Card> */}
-
+          <br></br>
+          <br></br>
           <Card>
             <Card.Body>
               <Card.Title>*AI Log*</Card.Title>
