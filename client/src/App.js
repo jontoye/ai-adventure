898842b04a3e4ef3e7d74c0e5f89afb8a6e21526
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-23 03:13:38 -0400

    merge fix, re-added adventure.jsx lines

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import CreateCharacter from "./components/CreateCharacter";
 import CreateAdventure from "./components/CreateAdventure";
+import Adventure from "./components/Adventure";
 import "./App.scss";
 
 export default class App extends Component {
@@ -121,88 +122,90 @@ export default class App extends Component {
         ) : null;
         const { isAuth } = this.state;
         return (
-            <Router>
-                <Navbar fixed="top" variant="dark" bg="dark" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="/">| AI Adventure |</Navbar.Brand>
+          <Router>
+            <Navbar fixed='top' variant='dark' bg='dark' expand='lg'>
+              <Container>
+                <Navbar.Brand href='/'>| AI Adventure |</Navbar.Brand>
 
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                {isAuth ? (
-                                    <>
-                                        <Nav.Link href="/">Home</Nav.Link>
-                                        <Nav.Link href="/create-character">
-                                            Create Character
-                                        </Nav.Link>
-                                        <Nav.Link href="/create-adventure">
-                                            Create Adventure
-                                        </Nav.Link>
-                                        <Nav.Link
-                                            href="/signout"
-                                            onClick={this.logoutHandler}
-                                        >
-                                            Sign Out
-                                        </Nav.Link>
-                                    </>
-                                ) : (
-                                    <>
-                                        <Nav.Link href="/signup">
-                                            Sign Up
-                                        </Nav.Link>
-                                        <Nav.Link href="/signin">
-                                            Sign In
-                                        </Nav.Link>
-                                    </>
-                                )}
-                            </Nav>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                  <Nav className='me-auto'>
+                    {isAuth ? (
+                      <>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href='/create-character'>
+                          Create Character
+                        </Nav.Link>
+                        <Nav.Link href='/create-adventure'>
+                          Create Adventure
+                        </Nav.Link>
+                        <Nav.Link href='/signout' onClick={this.logoutHandler}>
+                          Sign Out
+                        </Nav.Link>
+                      </>
+                    ) : (
+                      <>
+                        <Nav.Link href='/signup'>Sign Up</Nav.Link>
+                        <Nav.Link href='/signin'>Sign In</Nav.Link>
+                      </>
+                    )}
+                  </Nav>
 
-                            <span id="main-greeting">
-                                {this.state.user
-                                    ? "Welcome " + this.state.user.user.name
-                                    : null}{" "}
-                            </span>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+                  <span id='main-greeting'>
+                    {this.state.user
+                      ? "Welcome " + this.state.user.user.name
+                      : null}{" "}
+                  </span>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
 
-                {message}
-                {failMessage}
-                {successMessage}
+            {message}
+            {failMessage}
+            {successMessage}
 
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            isAuth ? (
-                                <Home createRandomCharacter={this.createRandomCharacter}/>
-                            ) : (
-                                <Signin login={this.loginHandler} />
-                            )
-                        }
-                    ></Route>
-                    <Route
-                        path="/create-character"
-                        exact
-                        element={<CreateCharacter randomCharacter={this.state.randomCharacter}/>}
-                    />
-                    <Route
-                        path="/create-adventure"
-                        exact
-                        element={<CreateAdventure/>}
-                    />
-                    <Route
-                        path="/signup"
-                        element={<Signup register={this.registerHandler} />}
-                    ></Route>
-                    <Route
-                        path="/signin"
-                        element={<Signin login={this.loginHandler} />}
-                    ></Route>
-                </Routes>
+            <Routes>
+              <Route
+                path='/'
+                element={
+                  isAuth ? (
+                    <Home createRandomCharacter={this.createRandomCharacter} />
+                  ) : (
+                    <Signin login={this.loginHandler} />
+                  )
+                }
+              ></Route>
+              <Route
+                path='/create-character'
+                exact
+                element={
+                  <CreateCharacter
+                    randomCharacter={this.state.randomCharacter}
+                  />
+                }
+              />
+              <Route
+                path='/create-adventure'
+                exact
+                element={<CreateAdventure />}
+              />
+              <Route
+                path='/adventure'
+                exact
+                element={<Adventure />}
+              />
+              <Route
+                path='/signup'
+                element={<Signup register={this.registerHandler} />}
+              ></Route>
+              <Route
+                path='/signin'
+                element={<Signin login={this.loginHandler} />}
+              ></Route>
+            </Routes>
 
-                <Footer />
-            </Router>
+            <Footer />
+          </Router>
         );
     }
 }
--- client/src/components/Adventure.jsx
+++ client/src/components/Adventure.jsx
@@ -1,9 +1,291 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import Axios from "axios";
+import Log from "./Log";
+import "./Adventure.css";
+
+const { Configuration, OpenAIApi } = require("openai");
 
 export default class Adventure extends Component {
+  constructor() {
+    super();
+    this.state = {
+      placeholder: "Click to generate a new adventure!",
+      response: "",
+      newAdventure: {},
+      characters: [],
+      stories: [],
+      log: [],
+      prompt: "",
+      option1: "",
+      option2: "",
+      option3: "",
+    };
+  }
+
+  componentDidMount() {
+    this.loadCharacterList();
+    this.loadStoryList();
+  }
+
+  populateOptions = (option) => {
+    if (
+      option[0] === 1 ||
+      option[1] === 1 ||
+      option[2] === 1 ||
+      option[3] === 1
+    ) {
+      this.setState({ option1: option });
+    } else if (
+      option[0] === 2 ||
+      option[1] === 2 ||
+      option[2] === 2 ||
+      option[3] === 2
+    ) {
+      this.setState({ option2: option });
+    } else if (
+      option[0] === 3 ||
+      option[1] === 3 ||
+      option[2] === 3 ||
+      option[3] === 3
+    ) {
+      this.setState({ option3: option });
+    }
+  };
+
+  optionSelect = (option) => {
+    if (option === 1) {
+      this.setState({ log: [...this.state.log, this.state.option1] });
+    } else if (option === 2) {
+      this.setState({ log: [...this.state.log, this.state.option2] });
+    } else if (option === 3) {
+      this.setState({ log: [...this.state.log, this.state.option3] });
+    }
+  };
+
+  buttonOneClick = () => {
+    this.optionSelect(1);
+  };
+  buttonTwoClick = () => {
+    this.optionSelect(2);
+  };
+  buttonThreeClick = () => {
+    this.optionSelect(3);
+  };
+
+  loadCharacterList = () => {
+    // console.log("getting characters...");
+    Axios.get("character/index")
+      .then((response) => {
+        // console.log(response.data.characters);
+        this.setState({
+          characters: response.data.characters,
+        });
+      })
+      .catch((err) => {
+        console.log("Error fetching characters.");
+        console.log(err);
+      });
+  };
+
+  loadStoryList = () => {
+    // console.log("getting characters...");
+    Axios.get("/home")
+      .then((response) => {
+        // console.log(response.data.characters);
+        this.setState({
+          stories: response.data.characters,
+        });
+      })
+      .catch((err) => {
+        console.log("Error fetching characters.");
+        console.log(err);
+      });
+  };
+
+  handleChange = (event) => {
+    const attributeToChange = event.target.name; // this will give the name of the field that is changing
+    const newValue = event.target.value; //this will give the value of the field that is changing
+
+    const adventure = { ...this.state.newAdventure };
+    adventure[attributeToChange] = newValue;
+    // console.log("onchange", adventure);
+    this.setState({
+      newAdventure: adventure,
+    });
+  };
+
+  appendResponse = (response) => {};
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target),
+      formDataObj = Object.fromEntries(formData.entries());
+    // console.log(formDataObj);
+
+    let intro = `${formDataObj.characterStory}`;
+    let prompt = `Begin a ${formDataObj.genre} story to ${formDataObj.quest} in a ${formDataObj.setting} setting.`;
+    let AIprompt = intro + "\n" + prompt + "\n";
+    // let newPrompt = ''
+    // console.log("prompt test", AIprompt);
+    ////Open Ai Goes here
+
+    const configuration = new Configuration({
+      apiKey: process.env.REACT_APP_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    openai
+      .createCompletion("text-davinci-002", {
+        prompt: AIprompt,
+        temperature: 0.8,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+      .then((response) => {
+        let intro = response.data.choices[0].text;
+        if (intro[0] === "\n") {
+          intro = intro.slice(1, intro.length);
+        }
+        formDataObj.log = [AIprompt, response.data.choices[0].text];
+        this.setState({
+          newAdventure: formDataObj,
+          placeholder: `Adventure successfully created. Enjoy!`,
+          response: `${intro}`,
+          log: [
+            ...this.state.log,
+            intro,
+            prompt,
+            response.data.choices[0].text,
+          ],
+        });
+        this.addAdventure(formDataObj);
+      })
+      .catch((error) => {
+        console.log("error log:", error);
+      });
+    this.setState({
+      placeholder: `Generating Adventure. Please wait...`,
+      response: "",
+    });
+  };
+
   render() {
+    const characters = this.state.characters.map((c) => {
+      return (
+        <option value={c.backstory}>
+          {c.name} ({c.class})
+        </option>
+      );
+    });
+
     return (
-      <div>Adventure</div>
-    )
+      <div>
+        <Container>
+
+          <div className='game-log'>
+            <Log log={this.state.log} />
+          </div>
+          <Button variant='primary' size='lg' onClick={this.buttonOneClick}>
+            Button One - Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Est inventore debitis, repellat quis voluptates accusantium
+            molestiae, rem veniam suscipit quia esse, dicta molestias voluptate
+            sit! Commodi voluptatibus corporis voluptates optio!
+          </Button>
+          <br></br>
+          <br />
+          <Button variant='primary' size='lg' onClick={this.buttonTwoClick}>
+            Button Two - Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Est inventore debitis, repellat quis voluptates accusantium
+            molestiae, rem veniam suscipit quia esse, dicta molestias voluptate
+            sit! Commodi voluptatibus corporis voluptates optio!
+          </Button>
+          <br></br>
+          <br />
+          <Button variant='primary' size='lg' onClick={this.buttonThreeClick}>
+            Button Three - Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Est inventore debitis, repellat quis voluptates accusantium
+            molestiae, rem veniam suscipit quia esse, dicta molestias voluptate
+            sit! Commodi voluptatibus corporis voluptates optio!
+          </Button>
+          <br></br>
+          <br />
+          <Form onSubmit={this.onFormSubmit}>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Adventure name</Form.Label>
+              <Form.Control
+                type='text'
+                name='name'
+                placeholder='Monty Python and the Holy Grail'
+                onChange={this.handleChange}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Genre</Form.Label>
+              <Form.Select name='genre' onChange={this.handleChange}>
+                <option value='Action'>Action</option>
+                <option value='Fantasy'>Fantasy</option>
+                <option value='Historical Fiction'>Historical Fiction</option>
+                <option value='Memoir'>Memoir</option>
+                <option value='Mythic'>Mythic</option>
+                <option value='Romance'>Romance</option>
+                <option value='Science Fiction'>Science Fiction</option>
+                <option value='Thriller'>Thriller</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Setting</Form.Label>
+              <Form.Select name='setting' onChange={this.handleChange}>
+                <option value='Alternate Universe'>Alternate Universe</option>
+                <option value='Ancient Egypt'>Ancient Egypt</option>
+                <option value='Classical Greece'>Classical Greece</option>
+                <option value='Dystopia'>Dystopia</option>
+                <option value='Future'>Future</option>
+                <option value='Medieval'>Medieval</option>
+                <option value='Modern'>Modern</option>
+                <option value='Roman Empire'>Roman Empire</option>
+                <option value='Prehistoric'>Prehistory</option>
+                <option value='Steampunk'>Steampunk</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Length</Form.Label>
+              <Form.Select name='length' onChange={this.handleChange}>
+                <option value='very short'>Short Story</option>
+                <option value='short'>Novelette</option>
+                <option value='moderate'>Novella</option>
+                <option value='long'>Novel</option>
+                <option value='very long'>Epic</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>Character</Form.Label>
+              <Form.Select name='characterStory' onChange={this.handleChange}>
+                {characters}
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId=''>
+              <Form.Label className='text-white'>
+                What is your quest?
+              </Form.Label>
+              <Form.Control
+                type='text'
+                name='quest'
+                placeholder='Find the Holy Grail'
+                onChange={this.handleChange}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Text>{this.state.placeholder}</Form.Text>
+          </Form>
+          <br />
+          <br></br>
+          <br></br>
+        </Container>
+      </div>
+    );
   }
 }
