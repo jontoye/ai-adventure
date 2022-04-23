70c28f73059a5074cf502742e64ae2ff33191404
Runite Drill <camielplaysgames@gmail.com>
2022-04-22 23:22:38 -0700

    add quickstart functionality and textbox editing

---
+++ client/src/data/character.js
@@ -0,0 +1,23 @@
+export const TITLE = [
+    "", "Sir ", "Mr ", "Lady ", "Duchess "
+]
+
+export const NAME = [
+    "Sam", "John", "Elizabeth", "Saad"
+]
+
+export const DECOR = [
+    "", " the Brave", " the Bold", " the Sly", " the Gifted"
+]
+
+export const CLASS = [
+    "Busker", "Stock Trader", "Warrior", "Archer"
+]
+
+export const ABILITY = [
+    "Flying", "Foresight", "Invisibility", "Pickpocketing"
+]
+
+export const WEAKNESS = [
+    "Gluttony", "Napping", "Kryptonite", "Spiders"
+]
