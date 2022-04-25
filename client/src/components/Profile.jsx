db1593929d5d8cd9980ed0bd78bbd1e066a377d1
Jonathan Toye <jontoye@gmail.com>
2022-04-25 01:45:41 -0400

    update profile page skeleton

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './css/Profile.css'
 
 function Profile({ user }) {
@@ -14,9 +15,28 @@ function Profile({ user }) {
           <h4>0 Friends</h4>
         </div>
       </div>
-      <div className="row">
+      <div className="row mb-5"> 
+        <div className="col-8 mx-auto d-flex justify-content-between">
+          <Link to="/characters" className="display-6">My Characters</Link>
+          <Link to="#" className="display-6">My Adventures</Link>
+        </div>
+      </div>
+      <div className="row text-center">
         <div className="col-6 mx-auto">
+          <div className="display-6">
+            Biography
+          </div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloribus, beatae minus aliquam facere hic animi accusamus neque libero, molestias minima ipsa debitis molestiae veritatis obcaecati iste quod et aspernatur?</p>
+          <div className="display-6">
+            Activity
+          </div>
+          <p>Updated adventure [name] (3 mins ago)</p>
+          <p>Created adventure [name] (yesterday)</p>
+          <p>Created character [name] (2 days ago)</p>
+          <div className="display-6">
+            Achievements
+          </div>
+
         </div>
       </div>
     </div>
