8810c2142238d4027edb4349dc1e38ed2511db57
Runite Drill <camielplaysgames@gmail.com>
2022-04-26 10:04:56 -0700

    event create backend

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,4 +1,21 @@
+const Event = require("../models/Event");
+
+exports.event_create_post = (req, res) => {
+  let event = new Event(req.body);
+
+  //save adventure
+  event.save()
+  .then((event) => {
+    res.json({ event });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.send("Error 418");
+  });
+};
+
 //HTTP GET - load stock detail
 exports.event_detail_get = (req, res) => {
     return
 }
+
--- server/routes/event.js
+++ server/routes/event.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 const eventCtrl = require("../controllers/event");
 router.get("/detail", eventCtrl.event_detail_get);
+router.post("/event/add", eventCtrl.event_create_post);
 
 module.exports = router;
