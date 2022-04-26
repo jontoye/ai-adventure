93a06f37f7ca6063c958a8da3e270d7ebd47848d
Jon Cannata <joncannats@gmail.com>
2022-04-26 19:11:59 -0400

    fix signout router

--- client/src/App.js
+++ client/src/App.js
@@ -181,27 +181,29 @@ export default class App extends Component {
       });
   };
 
+    logoutHandler = (e) => {
+    e.preventDefault();
+
+    localStorage.removeItem("token");
+    const redirect = () =>{
+        window.location.href = '/'
+    }
+    redirect()
+    this.setState({
+      isAuth: false,
+      user: null,
+      isGoogleUser: false,
+      message: null,
+      failMessage: "User logged out successfully.",
+      successMessage: null,
+      logoutRedirect: true,
+    });
+    
+  };
+
   render() {
 
-    const logoutHandler = (e) => {
-        e.preventDefault();
-
-        localStorage.removeItem("token");
-        const redirect = () =>{
-            window.location.href = '/'
-        }
-        redirect()
-        this.setState({
-          isAuth: false,
-          user: null,
-          isGoogleUser: false,
-          message: null,
-          failMessage: "User logged out successfully.",
-          successMessage: null,
-          logoutRedirect: true,
-        });
-        
-      };
+    
 
     const message = this.state.message ? (
       <Alert variant="info">{this.state.message}</Alert>
@@ -266,7 +268,7 @@ export default class App extends Component {
                     <Link
                       to="/signout"
                       className="nav-link"
-                      onClick={logoutHandler}
+                      onClick={this.logoutHandler}
                     >
                       Sign Out
                     </Link>
