ba402e615ccdaa53efd671b1b6f0949e77b6873e
Jonathan Toye <jontoye@gmail.com>
2022-04-28 00:17:13 -0400

    add click functions for horizontal scrolling, style updates

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -22,30 +22,50 @@ function Users({ continueAdventure, createAdventure, setCharacter }) {
     getUsers();
   }, []);
 
+  const scrollLeft = () => {
+    document.querySelector('.users-list').scrollLeft += 500
+  }
+
+  const scrollRight = () => {
+    document.querySelector('.users-list').scrollLeft -= 500
+  }
+
   return (
     <>
-      <div className='section-explore container my-5'>
-        <h1>Active Users</h1>
+      <div className='section-explore container-xl my-5'>
+        <h1 className="display-4">Explore</h1>
 
         {/* <div className='users-list d-flex flex-column align-items-center gap-5 my-5'> */}
-        <div className='users-list d-flex align-items-center gap-5'>
-          {users.map(user => (
-            <Link to={`/profile/${user._id}/`} key={user._id}>
-              <div className='user-card'>
-                <div className='user-card__img'>
-                  <img
-                    className='circular-square'
-                    src={user.avatar}
-                    alt='user-avatar'
-                  />
-                </div>
-                  <div className='user-card__content mx-2'>
-                    <h3>{user.username}</h3>
-                    <p>Joined {moment(user.createdAt).fromNow()}</p>
+        <div className="d-flex align-items-center">
+          <img 
+            className="scroll-btn" 
+            src="/images/icons/left-arrow.png" 
+            onClick={scrollRight} 
+            alt="left-arrow" />
+          <div className='users-list d-flex align-items-center gap-5'>
+            {users.map(user => (
+              <Link to={`/profile/${user._id}/`} key={user._id}>
+                <div className='user-card'>
+                  <div className='user-card__img'>
+                    <img
+                      className='circular-square'
+                      src={user.avatar}
+                      alt='user-avatar'
+                    />
                   </div>
-              </div>
-            </Link>
-          ))}
+                    <div className='user-card__content mx-2'>
+                      <h3>{user.username}</h3>
+                      <p>Joined {moment(user.createdAt).fromNow()}</p>
+                    </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+          <img 
+            className="scroll-btn" 
+            src="/images/icons/right-arrow.png" 
+            onClick={scrollLeft} 
+            alt="right-arrow" />
         </div>
       </div>
 
