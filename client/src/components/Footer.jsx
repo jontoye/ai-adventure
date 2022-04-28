c1a9d8ff1aaf111174fc5905e294cdeb0f6c2f2b
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-28 03:15:38 -0400

    minor style/tutorial changes saved before bed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import './css/Footer.css'
+import React, { Component } from "react";
+import "./css/Footer.css";
 
 export default class Footer extends Component {
   render() {
     return (
       <footer className='main-footer'>
-          <h5>AI Adventure</h5>
-          <p>Learn more about <a href="https://openai.com/">OpenAI</a></p>
-          <a href="/#contact">Contact Us</a>
-
+        <h5>AI Adventure</h5>
+        <div>
+          Learn more about <a href='https://openai.com/'>OpenAI</a>
+        </div>
+        <a href='/#contact'>Contact Us</a>
       </footer>
-    )
+    );
   }
 }
--- client/src/components/Tutorial.jsx
+++ client/src/components/Tutorial.jsx
@@ -1,27 +1,48 @@
-import React, { Component } from 'react'
-import { Container } from 'react-bootstrap'
-import './css/Tutorial.css'
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import "./css/Tutorial.css";
 
 export default class Tutorial extends Component {
-    render() {
-        return (
-            <Container className="section-tutorial" id="tutorial">
-                <div className="tutorial__content">
-                    <h1 className="tutorial__title">Tutorial</h1>
-                    <h2 className="tutorial__heading">What is AI Adventure?</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint culpa mollitia itaque eos dolore suscipit vero reprehenderit. Non vitae deleniti tempore architecto deserunt quasi ipsam cumque repudiandae quisquam! Quae?</p>
-                </div>
+  render() {
+    return (
+      <Container className='section-tutorial' id='tutorial'>
+        <div className='tutorial__content'>
+          <h1 className='tutorial__title'>Tutorial</h1>
+          <h2 className='tutorial__heading'>What is AI Adventure?</h2>
+          <p>
+            AI Adventure is a choose your own adventure social hub where you can
+            create and share your characters and adventures.
+            <br /> AI Adventure is powered by GPT3, allowing you to effortlessly
+            create any type of adventure you can put in to words. <br />
+            All of the art on the site was made using the Disco Diffusion AI
+          </p>
+        </div>
 
-                <div className="tutorial__content">
-                    <h2 className="tutorial__heading">How do you play?</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat doloremque hic suscipit? Mollitia tempora commodi odio dignissimos quasi, ducimus aliquam eius est atque reiciendis quae doloribus odit. Eveniet, consectetur velit.</p>
-                </div>
+        <div className='tutorial__content'>
+          <h2 className='tutorial__heading'>How do you play?</h2>
+          <p>
+            To play you can press on the Quick Start button on the home page or
+            the Create Character button in the nav. <br /> You can randomize
+            your character until you are satisfied or create one from scratch on
+            your own.
+            <br /> After creating a character you can choose the setting/theme
+            of the story, decide your quest, and set out on a GPT3 powered AI
+            Adventure.
+          </p>
+        </div>
 
-                <div className="tutorial__content">
-                    <h3 className="tutorial__heading">This is amazing! But how does it work?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio cum magnam eum explicabo cupiditate facere expedita. Maxime, recusandae voluptatum? Aperiam vitae cumque unde esse delectus vero architecto, quod a suscipit.</p>
-                </div>
-            </Container>
-        )
+        <div className='tutorial__content'>
+          <h3 className='tutorial__heading'>
+            This is amazing! But how does it work?
+          </h3>
+          <p>
+            GPT-3 stands for Generative Pre-trained Transformer 3, and it is the
+            third version of the language model that Open AI released in May
+            2020. It is generative, as GPT-3 can generate long sentences of the
+            unique text as the output. <br /> It is essentially magic which we use to empower you to create your own magical adventures.
+          </p>
+        </div>
+      </Container>
+    );
   }
 }
