145a5e17a3e95a988aca561fda67b0126b0ae7e0
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-05-01 11:57:00 -0400

    json size increase

--- server/server.js
+++ server/server.js
@@ -16,7 +16,11 @@ const app = express();
 app.use(timeout("29s"));
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static("public"));
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "20mb",
+  })
+);
 app.use(cors());
 
 app.use(haltOnTimedout);
