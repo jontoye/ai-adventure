5110927217be540b848f43621666d9a266902f00
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-05-01 15:32:26 -0400

    removed button text from bottom of log

--- client/src/components/Entry.jsx
+++ client/src/components/Entry.jsx
@@ -2,11 +2,35 @@ import React, { Component } from "react";
 import "./css/Entry.css";
 
 export default class Entry extends Component {
-  key = `entry ${this.props.colorID}`;
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      key: `entry ${this.props.colorID}`,
+    };
+  }
+
+  componentDidMount() {
+    console.log("[0]", this.props.text[0]);
+    this.handleOptions();
+  }
+
+  handleOptions() {
+    if (this.props.text[0] === "1") {
+      this.key = `${this.key} hidden-log`;
+      this.setState({
+        key: `entry ${this.props.colorID} hidden-log`,
+      });
+      console.log("this key", this.key);
+    } else {
+      this.setState({
+        key: `entry ${this.props.colorID}`,
+      });
+    }
+  }
   render() {
     return (
-      <div className={this.key}>
+      <div className={this.state.key}>
         <div className='entryText typewriter'>
           {this.props.text.replace(/(<([^>]+)>)/gi, "")}
         </div>
